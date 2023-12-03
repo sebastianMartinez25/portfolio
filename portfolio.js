f5dbf3cd645784ef9797800885bae15c806fdd92
sebastianMartinez25 <sebastianmartinez25065@gmail.com>
2023-12-02 21:15:28 -0500

    version0.0.2 date and total portfolio

--- portfolio.js
+++ portfolio.js
@@ -17,14 +17,14 @@ const vue = new Vue({
   //// nuestra      APIKey
   apiKey = 'AIzaSyD4TFfvj5gmSy1ndPFA9GxO8LOulEyk-ik'; 
   // rango de la hoja de calculo que queremos leer
-  values = 'A2:B10000';
+  values = 'A2:F10000';
  // fetch es un método nativo para hacer peticiones http
  // en el navegador 
   fetch("https://content-sheets.googleapis.com/v4/spreadsheets/" +   idSheets + "/values/"+values+"?access_token="+ apiKey +"&key="+  apiKey)
  .then((lista)=>{
    return lista.json()
  }).then((valores)=>{
-   this.listaDatos = valores.values.reverse();
+   this.listaDatos = valores.values;
           })
           .catch(err=>{
             console.log(err);})
